98fe04d5cc7b04e6877f0f512d101519ecae21e0
　”liyang“ <liyang.ok@outlook.com>
2022-01-04 08:10:27 +0800

    init

---
+++ AnArch/src/main/java/com/top/arch/annotation/Test.java
@@ -0,0 +1,19 @@
+package com.top.arch.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+
+
+    int max() default 0;
+
+    int min() default 0;
+
+    String description() default "";
+}
+
---
+++ Androidx/src/main/java/com/top/androidx/widget/CTextView.java
@@ -0,0 +1,11 @@
+package com.top.androidx.widget;
+
+import android.widget.EditText;
+
+public class CTextView {
+
+    public CTextView() {
+        //EditText editText=new EditText();
+
+    }
+}
